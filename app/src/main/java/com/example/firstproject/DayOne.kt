d9084993b8035b838d632eb1d1b04153a0448479
Ishan <ishanjoshi234@gmail.com>
2025-11-04 14:26:38 +0545

    First commit

---
+++ app/src/main/java/com/example/firstproject/DayOne.kt
@@ -0,0 +1,28 @@
+package com.example.firstproject
+
+import android.R
+
+fun main(){
+//    println("Hello")
+//    println("World")
+//    //mutable
+//    var name : String = "Ishan"
+//    //immutable
+//    val age : Int = 15
+//    println("My name is ${name.uppercase()}")
+val age = arrayOf(10,20,30)
+    age[3] = 50
+    println(age[3])
+
+    val age1 = ArrayList<Int>()
+    val age2 = arrayListOf<Int>()
+
+
+}
+fun calculate(a: Int, b: Int){
+
+}
+
+
+
+
