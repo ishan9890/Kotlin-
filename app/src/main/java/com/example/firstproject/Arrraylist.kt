763aca593a6187ea84f24099864221f0cfcad46b
Ishan <ishanjoshi234@gmail.com>
2025-11-05 15:56:15 +0545

    Second commit

---
+++ app/src/main/java/com/example/firstproject/Arrraylist.kt
@@ -0,0 +1,10 @@
+package com.example.firstproject
+
+fun main(){
+    var age = arrayOf(1,2,3)
+    println(age)
+    println("The first element of age is " + age[0])
+    println("The second element of age is " + age[1])
+    println("The third element of age is " + age[2])
+
+}
--- app/src/main/java/com/example/firstproject/Variables.kt
+++ app/src/main/java/com/example/firstproject/Variables.kt
@@ -1,2 +1,14 @@
 package com.example.firstproject
 
+fun main() {
+    var a: String = "Hello world"
+    var i: Int = a.length
+    var x: Boolean = a.equals("Hello world")
+    var username: String = " softwarica "
+
+    println(username.trim())
+    println(a)
+    println(i)
+    println(a.isEmpty())
+    println(a.lowercase())
+}
